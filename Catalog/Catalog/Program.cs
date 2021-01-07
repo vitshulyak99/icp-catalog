31533855db5f260f75d8aa06276db9a80da4a920
vitshulyak99 <vitshu1999@gmail.com>
2021-01-07 03:53:47 +0300

    Meaningful commit message

--- Catalog/Catalog/Program.cs
+++ Catalog/Catalog/Program.cs
@@ -16,8 +16,16 @@ namespace Catalog
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var port = Environment.GetEnvironmentVariable("PORT");
+
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:" + port);
+                });
+        }
     }
 }
